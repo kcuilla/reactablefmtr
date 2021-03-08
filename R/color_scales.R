3333ef2ca864c55ec119bcd77745606f0a46bc4c
kcuilla <kyle.cuilla@gmail.com>
2021-03-08 10:45:43 -0500

    update span parameter in color_scales()

--- R/color_scales.R
+++ R/color_scales.R
@@ -14,8 +14,10 @@
 #'     Values with a dark-colored background will be shown in white.
 #'     Default is set to TRUE but can be turned off by setting to FALSE.
 #'
-#' @param span Optionally apply colors to values in relation to the entire dataset instead of by column.
-#'     Default is set to NULL but can be turned on by setting to TRUE.
+#' @param span Optionally apply colors to values across multiple columns instead of by each column.
+#'     To apply across all columns set to TRUE.
+#'     If applying to a set of columns, can provide either column names or column positions.
+#'     Default is set to FALSE.
 #'
 #' @return a function that applies conditional colors
 #'     to a column of numeric values.
@@ -58,17 +60,17 @@
 #' @export
 
 color_scales <- function(data, colors = c("#ff3030", "#ffffff", "#1e90ff"), bright_values = TRUE, span = FALSE) {
-  
+
   color_pal <- function(x) {
-    
+
     if (!is.na(x))
       rgb(colorRamp(c(colors))(x), maxColorValue = 255)
     else
       NULL
   }
-  
+
   assign_color <- function(x) {
-    
+
     if (!is.na(x)) {
       rgb_sum <- rowSums(colorRamp(c(colors))(x))
       color <- ifelse(rgb_sum >= 375, "black", "white")
@@ -76,54 +78,54 @@ color_scales <- function(data, colors = c("#ff3030", "#ffffff", "#1e90ff"), brig
     } else
       NULL
   }
-  
+
   style <- function(value, index, name) {
-    
+
     if (!is.numeric(value)) return(value)
-    
+
     if (is.logical(span)) {
-      
+
       if (span) {
-        
+
         normalized <- (value - min(dplyr::select_if(data, is.numeric), na.rm = TRUE)) / (max(dplyr::select_if(data, is.numeric), na.rm = TRUE) - min(dplyr::select_if(data, is.numeric), na.rm = TRUE))
-        
+
       } else {
-        
+
         normalized <- (value - min(data[[name]], na.rm = TRUE))/(max(data[[name]], na.rm = TRUE) - min(data[[name]], na.rm = TRUE))
-        
+
       }
-      
+
       cell_color <- color_pal(normalized)
       font_color <- assign_color(normalized)
-      
+
     } else if (is.numeric(span) | is.character(span)) {
-      
+
       if (all(span %in% which(sapply(data, is.numeric))) | all(span %in% names(which(sapply(data, is.numeric))))) {
-        
+
         if (is.character(span)) { span <- which(names(data) %in% span) }
-        
+
         normalized <- (value - min(dplyr::select(data, !!span), na.rm = TRUE)) / (max(dplyr::select(data, !!span), na.rm = TRUE) - min(dplyr::select(data, !!span), na.rm = TRUE))
         cell_color <- if (name %in% colnames(data)[span]) { color_pal(normalized) }
         font_color <- if (name %in% colnames(data)[span]) { assign_color(normalized) }
-        
+
       } else {
-        
+
         stop("Attempted to select non-existing or non-numeric columns with span")
-        
+
       }
-      
+
     }
-    
-    
+
+
     if (bright_values == FALSE) {
-      
+
       list(background = cell_color)
-      
+
     } else {
-      
+
       list(background = cell_color, color = font_color)
-      
+
     }
-    
+
   }
 }
